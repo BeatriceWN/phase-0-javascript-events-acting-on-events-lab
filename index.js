7e80de14140c6c07b9fe7f8f6174e0a031b2f5a7
Beatrice Wambui <bettyneema2@gmail.com>
2025-06-29 17:36:24 +0300

    JS events acting on events lab

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
-// Your code here
+const dodger = document.getelementbyid("dodger");
+
+function moveDodgerLeft() {
+  const leftNumbers = dodger.style.left.replace("px", "");
+  const left = parseInt(leftNumbers, 10);
+
+  if (left > 0) {
+    dodger.style.left = `${left - 1}px`;
+  }
+}
+
+// function to move the dodger to the right
+function moveDodgerRight() {
+  const leftNumbers = dodger.style.left.replace("px", "");
+  const left = parseInt(leftNumbers, 10);
+
+// prevent dodger from going beyond right edge i.e. 400px - 40px = 360px
+  if (left < 360) {
+    dodger.style.left = `${left + 1}px`;
+  }
+}
+
+// add event listener
+document.addEventListener("keydown", function (e) {
+if (e.key === "ArrowLeft") {
+    moveDodgerLeft();
+  }
+
+// add another condition
+else if (e.key === "ArrowRight") {
+    moveDodgerRight();
+  }
+});
+
